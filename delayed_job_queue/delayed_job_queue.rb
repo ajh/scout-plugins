5aaccebb9181b1911c41c1f63b608e19ca7f84a6
Andy Hartford <hartforda@gmail.com>
2009-10-08 13:05:03 -0700

    add more reports and refactor existing ones

--- delayed_job_queue/delayed_job_queue.rb
+++ delayed_job_queue/delayed_job_queue.rb
@@ -3,34 +3,64 @@ class DelayedJobQueue < Scout::Plugin
   def build_report
     begin
       require "#{@options['path_to_app']}/config/environment"
-    rescue
+    rescue Exception
       error "Couldn't load rails environment at #{@options['path_to_app'].inspect}.", $!.message
+      return
     end
 
-    report(:jobs_count => jobs_count)
-    jobs_count_by_priority.each { |priority,count|
-      # note: this only reports priorities of existing jobs
-      report(:"jobs_with_priority_#{priority}_count" => count)
-    }
-    report(:jobs_with_errors_count => jobs_with_errors_count)
-
-    # add age of oldest job
-    # add alerts for jobs with errors
+    report_jobs_count
+    report_jobs_count_by_priority
+    report_jobs_with_errors_count
+    report_age_of_oldest_job
+    alert_jobs_with_errors
   end
 
   private
 
-    # returns a hash where keys are priority and values are the counts.
-    def jobs_count_by_priority
-      results = Delayed::Job.all(:select => "priority, count(*) as count", :group => "priority")
-      results.inject({}) { |memo, dj| memo[dj.priority] = dj.count; memo}
+    def report_jobs_count
+      report :jobs_count => Delayed::Job.count
+    end
+    
+    def report_jobs_count_by_priority
+      results = Delayed::Job.all \
+        :select => "priority, count(*) as count", 
+        :group => "priority"
+
+      # note: this only reports priorities of existing jobs
+      results.each { |j|
+        report(:"jobs_with_priority_#{j.priority}_count" => j.count)
+      }
     end
 
-    def jobs_count
-      Delayed::Job.count
+    def report_jobs_with_errors_count
+      report(:jobs_with_errors_count => jobs_with_errors.length)
     end
 
-    def jobs_with_errors_count
-      Delayed::Job.count(:conditions => "last_error IS NOT NULL")
+    def jobs_with_errors
+      Delayed::Job.all \
+        :select => "id, last_error",
+        :conditions => "last_error IS NOT NULL"
+    end
+
+    def report_age_of_oldest_job
+      job = Delayed::Job.first \
+        :select => "created_at", 
+        :order => 'created_at ASC'
+
+      report :age_of_oldest_job => (Time.now - job.created_at)
+    end
+
+    def alert_jobs_with_errors
+      previous_errors = memory(:known_errors) || {}
+      known_errors = {}
+
+      jobs_with_errors.each do |j|
+        known_errors[j.id] = true
+        unless previous_errors[j.id]
+          alert "a job has encountered an error (id: #{j.id}).", j.last_error
+        end
+      end
+
+      remember(:known_errors => known_errors)
     end
 end
